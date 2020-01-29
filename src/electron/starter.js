50f64aae7a8687bf6cf42b93712b623161d813e8
Connor Younglund <cdy@airmail.cc>
2020-01-28 21:53:35 -0500

    setup electron build

--- src/electron/starter.js
+++ src/electron/starter.js
@@ -20,7 +20,7 @@ async function createWindow() {
   const startUrl = process.env.DEV
     ? 'http://localhost:3000'
     : url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
+        pathname: path.join(__dirname, '/../../build/index.html'),
         protocol: 'file:',
         slashes: true
       });
